a9105ffd55cd3e920046bd6954a920330cbf03e2
Tanmay Mone <84134747+tanmaymone@users.noreply.github.com>
2025-06-12 14:39:19 +0530

    adjusted the editnote window for better margin

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -14,11 +14,11 @@ function Note({ title, text, onClick, onDelete }) {
         delete
       </button>
 
-      <div className="note-card-body">
+      
         <div className="note-card-title">{title}</div>
         <div className="note-card-text">{text}</div>
       </div>
-    </div>
+    
   );
 }
 
